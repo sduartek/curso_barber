4e5010140d9190323d9b9f1e3fb1f9a60a30939d
Sergio Duarte <duartesergiok@gmail.com>
2026-02-17 11:53:17 -0300

    feat: ensure user can search barbershops

---
+++ barberapp/app/barbershops/page.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/app/_lib/prisma";
+import Header from "../_components/header";
+import BarbershopItem from "../(home)/_components/barbershop-item";
+
+interface BarbershopsPageProps {
+  searchParams: {
+    search?: string;
+  };
+}
+
+const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams.search,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h1 className="text-gray-400 font-bold text-xs uppercase">
+          Resultados para &quot;{searchParams.search}&quot;
+        </h1>
+        <div className="grid grid-cols-2 mt-3 gap-4">
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className="w-full">
+              <BarbershopItem barbershop={barbershop} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BarbershopsPage;
